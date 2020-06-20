1ff5a409f1af85e3542695bb93c23cc44e133595
David Perez <dperezval08@gmail.com>
2020-06-19 21:29:58 -0500

    Updates

---
+++ precache-manifest.cdd970a67c0d498b603d9f4ce21c7a48.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b01207c25bf49626ce11c03281ffbf3",
+    "url": "/layout-exercise-react/index.html"
+  },
+  {
+    "revision": "17ab84f70796fe4fba06",
+    "url": "/layout-exercise-react/static/css/main.8b1729e8.chunk.css"
+  },
+  {
+    "revision": "a7a98b973263e3b34ae9",
+    "url": "/layout-exercise-react/static/js/2.2da03d0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/layout-exercise-react/static/js/2.2da03d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ab84f70796fe4fba06",
+    "url": "/layout-exercise-react/static/js/main.94e4fbd6.chunk.js"
+  },
+  {
+    "revision": "bc1f0b80aaf50f6ea835",
+    "url": "/layout-exercise-react/static/js/runtime-main.f97bdf1f.js"
+  },
+  {
+    "revision": "00df629ed0fe3cf5048435e31f552744",
+    "url": "/layout-exercise-react/static/media/analisis.00df629e.jpg"
+  },
+  {
+    "revision": "1a98e68162f1bcff194ab7e436334e82",
+    "url": "/layout-exercise-react/static/media/apreton-de-manos.1a98e681.jpg"
+  },
+  {
+    "revision": "70401acffed77a892284bea676372bd2",
+    "url": "/layout-exercise-react/static/media/investigacion.70401acf.jpg"
+  },
+  {
+    "revision": "323c8a08e85bc1c5475045da917b15e0",
+    "url": "/layout-exercise-react/static/media/planta-en-manos.323c8a08.jpg"
+  }
+]);
